3eac896a5cd19a8b0abc1ecd39da6fe170f6e164
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-07-09 20:33:14 -0400

    add Access-Control-Allow-Origin header

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -5,6 +5,7 @@ const Yelp = {
  search(term, location, sortBy) {
   return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
     headers:{
+      'Access-Control-Allow-Origin':'https://sam-alefsen.github.io/ravenous',
       Authorization: `Bearer ${apiKey}`
     }
   }).then(response => {
